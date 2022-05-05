d8316d7154ba0d63153854085b606e1818520dfa
ryosukeohi <ryosuke.ohi@gmail.com>
2022-05-05 08:27:02 +0000

    [Finish]JS-13

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,34 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "skippr.min";
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+require('jquery')
+
+var title = "javascriptが使えました";
+alert(title);
+
+$(document).ready(function(){
+  $('.jquery').on('click', function(){
+    $(this).css('color', 'red');
+  });
+});
+
+$(document).ready(function(){
+  $('#theTarget').skippr({
+    transition: 'slide',
+    speed: 1000,
+    easing: 'easeOutQuart',
+    navType: 'block',
+    childrenElementType: 'div',
+    arrows: true,
+    autoPlay: true,
+    autoPlayDuration: 3000,
+    keyboardOnAlways: true,
+    hidePrevious: false
+  });
+});
